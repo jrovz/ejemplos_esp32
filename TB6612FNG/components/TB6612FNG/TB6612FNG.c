f2ab0f1e2ca7f41be59713090f1ca3558a741ea7
Juan Felipe Rodriguez Valencia <122070886+jrovz@users.noreply.github.com>
2023-10-23 08:20:02 -0500

    Update TB6612FNG.c

--- TB6612FNG/components/TB6612FNG/TB6612FNG.c
+++ TB6612FNG/components/TB6612FNG/TB6612FNG.c
@@ -1,12 +1,32 @@
 #include "TB6612FNG.h"
+#include "driver/ledc"
 
 void dcmotor_init(DCMotor* motor) {
+    // Configurar pines como salidas y establecer niveles iniciales
     gpio_set_direction(motor->in1, GPIO_MODE_OUTPUT);
     gpio_set_direction(motor->in2, GPIO_MODE_OUTPUT);
-    gpio_set_direction(motor->pwm, GPIO_MODE_OUTPUT);
     gpio_set_level(motor->in1, 0);
     gpio_set_level(motor->in2, 0);
-    gpio_set_level(motor->pwm, 0);
+
+    // Configurar el canal LEDC
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_DUTY_RES,
+        .freq_hz = LEDC_FREQUENCY,
+        .speed_mode = LEDC_MODE,
+        .timer_num = LEDC_TIMER
+    };
+    ledc_timer_config(&ledc_timer);
+
+    ledc_channel_config_t ledc_channel = {
+        .channel = LEDC_CHANNEL,
+        .duty = 0, // Iniciar con un ciclo de trabajo de 0
+        .gpio_num = motor->pwm,
+        .intr_type = LEDC_INTR_DISABLE,
+        .speed_mode = LEDC_MODE,
+        .timer_sel = LEDC_TIMER
+    };
+    ledc_channel_config(&ledc_channel);
+    ledc_fade_func_install(0);
 }
 
 esp_err_t dcmotor_set_speed(DCMotor* motor, uint8_t speed) {
@@ -14,43 +34,31 @@ esp_err_t dcmotor_set_speed(DCMotor* motor, uint8_t speed) {
         return ESP_ERR_INVALID_ARG;
     }
 
-    if (gpio_set_level(motor->in1, 1) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (gpio_set_level(motor->in2, 0) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (gpio_set_level(motor->pwm, 1) != ESP_OK) {
-        return ESP_FAIL;
+    // Configurar la dirección de los pines
+    gpio_set_level(motor->in1, 1);
+    gpio_set_level(motor->in2, 0);
+
+    // Establecer el ciclo de trabajo del canal LEDC
+    esp_err_t err = ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, speed);
+    if (err != ESP_OK) {
+        return err;
     }
 
-    if (ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, speed) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0) != ESP_OK) {
-        return ESP_FAIL;
-    }
-
-    return ESP_OK;
+    // Actualizar el ciclo de trabajo
+    return ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
 }
 
 esp_err_t dcmotor_brake(DCMotor* motor) {
-    if (gpio_set_level(motor->in1, 1) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (gpio_set_level(motor->in2, 1) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (gpio_set_level(motor->pwm, 0) != ESP_OK) {
-        return ESP_FAIL;
+    // Configurar la dirección de los pines para frenar
+    gpio_set_level(motor->in1, 1);
+    gpio_set_level(motor->in2, 1);
+
+    // Establecer el ciclo de trabajo del canal LEDC a 0
+    esp_err_t err = ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0);
+    if (err != ESP_OK) {
+        return err;
     }
 
-    if (ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0) != ESP_OK) {
-        return ESP_FAIL;
-    }
-    if (ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0) != ESP_OK) {
-        return ESP_FAIL;
-    }
-
-    return ESP_OK;
+    // Actualizar el ciclo de trabajo
+    return ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
 }
